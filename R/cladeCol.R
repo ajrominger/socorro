c5395f0df997c023af295cb145728bc93e5d65ab
ajrominger <ajrominger@gmail.com>
2016-10-17 17:11:42 -0700

    cleaned up function

--- R/cladeCol.R
+++ R/cladeCol.R
@@ -1,8 +1,26 @@
-cladeCol <- function(phy, # the phylogeny
-                      clades,	# a list of clades 
-                      cols,		# a vector of colors, one for each clade
-                      bg.col=par("fg")) {	# the color for ``background'' edges
-    sub.phy <- subtrees(phy)
+#' @title Create colors for plotting clades
+#'  
+#' @description Create a vector of colors for use in \code{plot.phylo} so that designated clades have a unique color
+#' 
+#' @details Clades are specified by the names of their members, see example
+#' 
+#' @param phy the phylogeny for which to compute colors
+#' @param clades a list of clades 
+#' @param cols a vector of colors, one for each clade
+#' @param bg.col color for ``background'' edges (i.e. those not specified by \code{clades})
+#' 
+#' @return A character vector of the desired colors
+#' 
+#' @examples
+#' phy <- read.tree(text="(A,(C,G));")
+#' plot(phy, edge.color=clade.col(phy, list("A",c("C","G")),c("red","blue")))
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>
+#' @seealso phylo, plot.phylo
+#' @export
+
+cladeCol <- function(phy, clades,cols, bg.col=par("fg")) {
+    sub.phy <- ape::subtrees(phy)
     
     match.clades <- sapply(sub.phy, function(x) {
         tips <- x$tip.label
@@ -15,7 +33,7 @@ cladeCol <- function(phy, # the phylogeny
             yintips & tipsiny
         })
         the.match <- which(match.tips)
-        if(length(the.match)==0) the.match <- 0
+        if(length(the.match) == 0) the.match <- 0
         
         the.match
     })
@@ -37,21 +55,16 @@ cladeCol <- function(phy, # the phylogeny
     }
     
     ## now must account for single taxon clades
-    these.sing <- which(sapply(clades,length)==1) # clade indeces
+    these.sing <- which(sapply(clades, length) == 1) # clade indeces
     print(these.sing)
     if(length(these.sing) > 0) {
         sing.tax <- unlist(clades[these.sing])	# taxon names
         sing.col <- cols[these.sing]
         
-        sing.tip.ind <- match(sing.tax,phy$tip.label)
+        sing.tip.ind <- match(sing.tax, phy$tip.label)
         
-        phy.col[match(sing.tip.ind,phy$edge[,2])] <- sing.col
+        phy.col[match(sing.tip.ind, phy$edge[,2])] <- sing.col
     }
     
     return(phy.col)
 }
-
-## test it
-# phy <- read.tree(text="(A,(C,G));")
-# plot(phy, edge.color=clade.col(phy, list("A",c("C","G")),c("red","blue")))
-
