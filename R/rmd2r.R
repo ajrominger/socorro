e0ae69fc11b95e89109a1fe0fa49542d7e649e00
ajrominger <ajrominger@gmail.com>
2017-04-19 15:29:25 -0600

    added functionality to better strip tags and MD text

--- R/rmd2r.R
+++ R/rmd2r.R
@@ -12,6 +12,8 @@
 #' @param x the Rmd file
 #' @param file the file to write to, defaults to same name as \code{x} but appended with '.R'
 #' @param noPlots logical, should plotting chunks be commented out
+#' @param rmTags logical, should code chunk tags be removed
+#' @param stripMD logical, should markdown text be removed
 #' 
 # @examples
 #'
@@ -19,7 +21,7 @@
 # @seealso
 #' @export
 
-rmd2r <- function(x, file, noPlots = TRUE) {
+rmd2r <- function(x, file, noPlots = TRUE, rmTags = TRUE, stripMD = FALSE) {
     ## deal with possible file input
     if(missing(file)) file <- gsub('\\.rmd', '.R', x, ignore.case = TRUE)
     if(!grepl('\\.R', file, ignore.case = TRUE)) file <- paste0(file, '.R')
@@ -37,35 +39,58 @@ rmd2r <- function(x, file, noPlots = TRUE) {
     actuallyStrt <- which(codeTag %in% rStrt)
     rEnd <- codeTag[actuallyStrt + 1]
     
-    ## if we don't want plotting chunks, remove them from rStrt and rEnd
-    # if(noPlots) {
-    #     plotsHere <- grep('plot', x)
-    #     if(length(plotsHere) > 0) {
-    #         browser()
-    #         rmThese <- sapply(rStrt, function(r) any(r < plotsHere)) & 
-    #             sapply(rEnd, function(r) any(r > plotsHere))
-    #         rStrt <- rStrt[!rmThese]
-    #         rEnd <- rEnd[!rmThese]
-    #     }
-    # }
-    
     ## add roxygen tag to everything
     x <- paste0("#' ", x)
     
     ## remove from R code chunks
     for(i in 1:length(rStrt)) {
-        if(noPlots) {
-            if(!any(grepl('plot|par|acf', x[(rStrt[i] + 1):(rEnd[i] + 1)]))) {
-                x[(rStrt[i] + 1):(rEnd[i] + 1)] <- 
-                    gsub("#' ", '', x[(rStrt[i] + 1):(rEnd[i] + 1)])
-                x[c(rStrt[i], rEnd[i])] <- ''
-            }
-        } else {
-            x[(rStrt[i] + 1):(rEnd[i] + 1)] <- 
-                gsub("#' ", '', x[(rStrt[i] + 1):(rEnd[i] + 1)])
-            x[c(rStrt[i], rEnd[i])] <- ''
+        x[(rStrt[i] + 1):(rEnd[i] - 1)] <- gsub("#' ", '', x[(rStrt[i] + 1):(rEnd[i] - 1)])
+        
+        if(rmTags) x[c(rStrt[i], rEnd[i])] <- ''
+    }
+    
+    if(noPlots) {
+        ## find plotting function indeces
+        plotInd <- .findPlots(x)
+        
+        ## find which chunks to comment out
+        comThese <- sapply(1:length(rStrt), function(i) any(rStrt[i]:rEnd[i] %in% plotInd))
+        pStrt <- rStrt[comThese]
+        pEnd <- rEnd[comThese]
+        
+        ## comment them out
+        for(i in 1:length(pStrt)) {
+            x[(pStrt[i] + 1):(pEnd[i] - 1)] <- paste0("# ", x[(pStrt[i] + 1):(pEnd[i] - 1)])
         }
     }
     
+    ## strip MD text if desired
+    if(stripMD) {
+        x[rStrt] <- ''
+        x <- x[!grepl("#' ", x)]
+    }
+    
     writeLines(x, con = file)
+}
+
+## helper function to find plotting functions
+.findPlots <- function(x) {
+    codeText <- getParseData(parse(text = x, keep.source=TRUE))
+    funs <- codeText[codeText$token == 'SYMBOL_FUNCTION_CALL', ]
+    
+    ## run needed code to make things work
+    src2run <- apply(funs[funs$text %in% c('setwd', 'source', 'library', 'require'), ], 1, 
+          function(src) x[src[1]:src[3]])
+    eval(parse(text = paste0(src2run, collapse = '\n')))
+    
+    plotFuns <- plotHere <- sapply(unique(funs$text), function(f) {
+        fun <- try(get(f), silent = TRUE)
+        if(class(fun) == 'function') {
+            return(any(grepl('plot|points', format(get(f)))))
+        } else {
+            return(FALSE)
+        }
+    })
+    
+    codeText$line1[codeText$text %in% names(plotFuns[plotFuns])]
 }
